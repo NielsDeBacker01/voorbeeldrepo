75cc5d8a8f90c8ff10dbddf879dc039a3ca3ac63
NielsDeBacker <niels.debacker01@student.ap.be>
2022-09-27 11:45:49 +0200

    merged

--- program.sh
+++ program.sh
@@ -2,8 +2,11 @@
 
 echo "Hello World!";
 <<<<<<< HEAD
+<<<<<<< HEAD
 echo "Feature 2";
 =======
+=======
+>>>>>>> 5727d12 (merged)
 read -p "Give your name: " name;
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -14,4 +17,10 @@ echo "Hello $name. Greetings to you my friend";
 >>>>>>> 1e3c4a0 (Add personal greeting to program)
 =======
 echo "Hello $name. Greetings to you my friend.";
+<<<<<<< HEAD
 >>>>>>> 0de444a (Fix typo)
+=======
+=======
+echo "Feature 2";
+>>>>>>> master
+>>>>>>> 5727d12 (merged)
